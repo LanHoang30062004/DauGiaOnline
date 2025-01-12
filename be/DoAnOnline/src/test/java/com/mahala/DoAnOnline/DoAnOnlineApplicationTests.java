5a643ddb7c923ada376ab173e6f04d7b3d66a196
MaiHoangLan <maihoanglan30@gmail.com>
2025-01-12 20:48:53 +0700

    second commit

---
+++ be/DoAnOnline/src/test/java/com/mahala/DoAnOnline/DoAnOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahala.DoAnOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
