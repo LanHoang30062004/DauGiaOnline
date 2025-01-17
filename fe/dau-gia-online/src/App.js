a2ee6c0a2724acd7218719f6b9298e203a9a39e5
MaiHoangLan <maihoanglan30@gmail.com>
2025-01-17 22:32:16 +0700

    yeu code

--- fe/dau-gia-online/src/App.js
+++ fe/dau-gia-online/src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './default/Header';
-import Footer from './default/Footer';
-import MyProfile from './component/MyProfile';
-import Register from './component/Register';
-import HelpContact from './component/HelpContact';
+
 
 function App() {
   return (
     <>
-    <Header/>
-    <Footer/>
+      
     </>
   );
 }
--- fe/dau-gia-online/src/component/Register/index.js
+++ fe/dau-gia-online/src/component/Register/index.js
@@ -3,56 +3,7 @@ import './register.css';
 
 function Register() {
   return (
-    <div className="d-flex align-items-center">
-      <div className="container">
-        <div className="row justify-content-center" style={{ margin: '20px' }}>
-          <div className="col-lg-6 col-md-8 register-box">
-            <div className="col-lg-12 register-title">
-              Register
-              <div className="register-line"></div>
-            </div>
-            <div className="col-lg-12 register-form">
-              <form>
-                <div className="row">
-                  {/* Bên trái */}
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input type="email" className="form-control" placeholder="Email" />
-                    </div>
-                    <div className="form-group">
-                      <input type="password" className="form-control" placeholder="Password" />
-                    </div>
-                    <div className="form-group">
-                      <input type="password" className="form-control" placeholder="Re-enter your password" />
-                    </div>
-                  </div>
-                  {/* Bên phải */}
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input type="text" className="form-control" placeholder="Full name" />
-                    </div>
-                    <div className="form-group">
-                      <input type="date" className="form-control" placeholder="Date" />
-                    </div>
-                    <div className="form-group">
-                      <input type="text" className="form-control" placeholder="Address" />
-                    </div>
-                  </div>
-                </div>
-                <div className="col-12 login-btm login-button justify-content-center d-flex">
-                  <button type="submit" className="btn-outline-primary">Register</button>
-                </div>
-                <div className="login-link text-center mt-3">
-                  <p>
-                    Back to Login? <a href="#">Login</a>
-                  </p>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <></>
   );
 }
 
