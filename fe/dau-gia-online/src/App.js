7d25625259a4dc37c676786d71005cb527fa8740
KZero27 <phamanhkhoazero27@gmail.com>
2025-01-18 00:18:55 +0700

    Pham Anh Khoa cap nhat footer, style chua lam xong. Chieu mai se lam

--- fe/dau-gia-online/src/App.js
+++ fe/dau-gia-online/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-<<<<<<< HEAD
-function App() {
-  return (
-    <>
-    
-=======
+import MyProfile from './component/MyProfile';
 
 function App() {
   return (
     <>
->>>>>>> 4129dd2eb74cfcf2e8f3d92bdfae7093db40b156
+      <MyProfile/>
     </>
-  );
+  )
 }
 
 export default App;
