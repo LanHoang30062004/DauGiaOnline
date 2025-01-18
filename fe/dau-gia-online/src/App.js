fa5b66d5597362a8a567704bfc1f3eb886461dad
Ngo Hong Ky <kingohong@gmail.com>
2025-01-18 15:26:28 +0700

    Hong Ky add full file Login

--- fe/dau-gia-online/src/App.js
+++ fe/dau-gia-online/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Login from './component/Login';
 
 
 function App() {
   return (
     <>
-      
     </>
   );
 }
--- fe/dau-gia-online/src/component/Login/index.js
+++ fe/dau-gia-online/src/component/Login/index.js
@@ -1,9 +1,35 @@
 import "./login.css"
-function Login () {
+function Login() {
     return (
         <>
-       
+            <div>
+                <header>
+                    <div className="header-content">
+                        <img src="/Logo.jpg" alt="Logo" className="header-logo"/>
+                            <h1 className="header-title">Login</h1>
+                    </div>
+                </header>
+                <div className="container">
+                    <h1>Login</h1>
+                    <form className="post">
+                        <div className="khung">
+                            <input type="email" className="Email" required />
+                            <span></span>
+                            <label>Email</label>
+                        </div>
+                        <div className="khung">
+                            <input type="password" required />
+                            <span></span>
+                            <label>Password</label>
+                        </div>
+                        <input type="submit" value="Login" />
+                            <div className="signup_link">
+                                Not a member? <a href="#">Signup</a>
+                            </div>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
-export default Login ; 
+export default Login; 
