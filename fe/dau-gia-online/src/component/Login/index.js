0e6f34bc3e4de772bb534a53c32e102cf2350aa8
MaiHoangLan <maihoanglan30@gmail.com>
2025-02-08 23:45:50 +0700

    Lan xu ly logic FE

--- fe/dau-gia-online/src/component/Login/index.js
+++ fe/dau-gia-online/src/component/Login/index.js
@@ -1,31 +1,52 @@
+import { useState } from "react";
 import "./login.css"
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const token = "fake";
+        login(token) ; 
+        navigate("/home") ; 
+    }
+
     return (
         <>
             <div>
                 <header>
                     <div className="header-content">
-                        <img src="/Logo.jpg" alt="Logo" className="header-logo"/>
-                            <h1 className="header-title">Login</h1>
+                        <img src="/Logo.jpg" alt="Logo" className="header-logo" />
+                        <h1 className="header-title">Login</h1>
                     </div>
                 </header>
                 <div className="container">
                     <h1>Login</h1>
-                    <form className="post">
+                    <form onSubmit={handleSubmit} className="post">
                         <div className="khung">
-                            <input type="email" className="Email" required />
+                            <input type="email" className="Email" required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                             <span></span>
                             <label>Email</label>
                         </div>
                         <div className="khung">
-                            <input type="password" required />
+                            <input type="password" required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                             <span></span>
                             <label>Password</label>
                         </div>
                         <input type="submit" value="Login" />
-                            <div className="signup_link">
-                                Not a member? <a href="#">Signup</a>
-                            </div>
+                        <div className="signup_link">
+                            Not a member? <a href="#">Signup</a>
+                        </div>
                     </form>
                 </div>
             </div>
