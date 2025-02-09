a025ec1b5bb69dad4b2ab1598e96be6032913c10
Ngo Hong Ky <kingohong@gmail.com>
2025-02-09 11:28:58 +0700

    Ky upload file code UpdateUsers

--- fe/dau-gia-online (ADMIN)/src/component/AddUser/index.js
+++ fe/dau-gia-online (ADMIN)/src/component/AddUser/index.js
@@ -1,7 +1,76 @@
 import "./style.css"
-function AddUser () {
+import { FaUsers } from "react-icons/fa";
+import { FaBoxArchive } from "react-icons/fa6";
+import React, { useState } from "react";
+const AddUser = () => {
+    const [user, setUser] = useState({
+        name: 'Megan Fox',
+        email: 'meganfox69@example.com',
+        date: '16/05/1986',
+        address: '190 Dien Bien Phu, Da Nang, Viet Nam',
+        balance: '8.420.030.000 VND'
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser((prevUser) => ({
+            ...prevUser,
+            [name]: value
+        }));
+    };
+
+    const handleUpdate = () => {
+        alert('User information updated');
+    };
+
+    const handleCancel = () => {
+        alert('Update canceled');
+    };
+
     return (
-        <></>
-    )
-}
-export default AddUser ; 
+        <div className="app">
+            <div className="sidebar">
+                <div className="logo">
+                    <h1>ADMIN</h1>
+                </div>
+                <ul className="menu">
+                    <li className="menu-item active">
+                        <span className="icon"><FaUsers /></span>
+                        <span className="text">Users</span>
+                    </li>
+                    <li className="menu-item">
+                        <span className="icon"><FaBoxArchive /></span>
+                        <span className="text">Products</span>
+                    </li>
+                </ul>
+                <div className="sidebar-user">
+                    <img src="/megan-fox-avatar.jpg" alt="User Avatar" className="avatar" />
+                    <p className="username">Megan Fox</p>
+                </div>
+            </div>
+            <div className="main-content">
+                <div className="header">
+                    <h1>UPDATE USERS</h1>
+                </div>
+                <div className="user-update">
+                    <div className="user-details">
+                        <div className="user-column">
+                            <input type="text" name="name" value={user.name} onChange={handleChange} />
+                            <input type="email" name="email" value={user.email} onChange={handleChange} />
+                            <input type="text" name="date" value={user.date} onChange={handleChange} />
+                        </div>
+                        <div className="user-column">
+                            <input type="text" name="address" value={user.address} onChange={handleChange} />
+                            <input type="text" name="balance" value={user.balance} onChange={handleChange} />
+                        </div>
+                    </div>
+                    <div className="user-actions">
+                        <button onClick={handleCancel}>Cancel</button>
+                        <button onClick={handleUpdate}>Update</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default AddUser; 
