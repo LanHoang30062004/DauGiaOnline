a265d35404ab8f60f2626df9e66010af68bc8cbd
LeHoaHiep <lehoahiepk98@gmail.com>
2025-02-09 23:25:17 +0700

    Hiep sua file (Admin)

--- fe/dau-gia-online (ADMIN)/src/component/AddProduct/index.js
+++ fe/dau-gia-online (ADMIN)/src/component/AddProduct/index.js
@@ -2,7 +2,6 @@ import "./style.css";
 import { FaUsers } from "react-icons/fa";
 import { MdInventory2 } from "react-icons/md";
 import { FaMagnifyingGlass } from 'react-icons/fa6';
-import { FaUsers } from "react-icons/fa";
 import { FaBoxArchive } from "react-icons/fa6";
 import React, { useState } from "react";
 function AddProduct() {
@@ -14,48 +13,55 @@ function AddProduct() {
                         <h1>ADMIN</h1>
                     </div>
                     <ul className="menu">
-                        <li className="menu-item active">
+                        <li className="menu-item ">
                             <span className="icon"><FaUsers /></span>
                             <span className="text">Users</span>
                         </li>
-                        <li className="menu-item">
+                        <li className="menu-item active">
                             <span className="icon"><FaBoxArchive /></span>
                             <span className="text">Products</span>
                         </li>
                     </ul>
                     <div className="sidebar-user">
                         <img src="/Megan Fox-avatar.jpg" alt="User Avatar" className="avatar" />
-                        <p className="username">Megan Fox</p>
+                        <a href="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online" className="username">Megan Fox</a>
                     </div>
                 </div>
                 <div className="main-box">
                     <div className="title">ADD PRODUCTS</div>
                     <div className="content-box">
-                        <div className="infor-box">
-                            <div className="name field">
-                                <input type="text" id="name" name="name" placeholder="Name product" />
-                            </div>
-                            <div className="price field">
-                                <input type="text" id="price" name="price" placeholder="Starting price" />
-                            </div>
+                        <form type="text" action="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online" method="POST">
+                            <div className="infor-box">
+                                <div className="name field">
+                                    <input type="text" id="name" name="name" placeholder="Name product" />
+                                </div>
+                                <div className="price field">
+                                    <input type="text" id="price" name="price" placeholder="Starting price" />
+                                </div>
 
-                            <div className="time field">
-                                <input type="text" id="time" name="time" placeholder="Auction time" />
+                                <div className="time field">
+                                    <input type="text" id="time" name="time" placeholder="Auction time" />
+                                </div>
+                                <div className="cate field">
+                                    <label for="cate">Category:</label>
+                                    <select id="cate" name="cate">
+                                        <option value="disabled selected"></option>
+                                        <option value="bag">Bag</option>
+                                        <option value="watch">Watch</option>
+                                        <option value="antique">Antique</option>
+                                        <option value="sneakers">Sneakers</option>
+                                    </select>
+                                </div>
                             </div>
-                            <div className="cate field">
-                                <label for="cate">Category:</label>
-                                <select id="cate" name="cate">
-                                    <option value="disabled selected"></option>
-                                    <option value="bag">Bag</option>
-                                    <option value="watch">Watch</option>
-                                    <option value="antique">Antique</option>
-                                    <option value="sneakers">Sneakers</option>
-                                </select>
-                            </div>
-                        </div>
+                        </form>
                         <div className="image-box">
                             <div className="img-area">
-                                <a className="upload-img" href="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online">+</a>
+                                <form action="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online" method="POST">
+                                    <label class="file-upload">
+                                        +
+                                        <input className="upload-img" type="file" id="fileInput"/>
+                                    </label>
+                                </form>
                             </div>
                             <a className="refre-btn" href="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online">Refresh</a>
                         </div>
--- fe/dau-gia-online (ADMIN)/src/component/UpdateProduct/index.js
+++ fe/dau-gia-online (ADMIN)/src/component/UpdateProduct/index.js
@@ -1,53 +1,67 @@
-import "./style.css"
+import "./style.css";
 import { FaUsers } from "react-icons/fa";
 import { MdInventory2 } from "react-icons/md";
 import { FaMagnifyingGlass } from 'react-icons/fa6';
-function UpdateProduct() {
+import { FaBoxArchive } from "react-icons/fa6";
+import React, { useState } from "react";
+function AddProduct() {
     return (
         <>
             <div className="ui">
-                <div className="sel-box">
+                <div className="sidebar">
                     <div className="logo">
-                        <img src={process.env.PUBLIC_URL + "/logo-admin.png"} alt="" />
+                        <h1>ADMIN</h1>
                     </div>
-                    <div className="user-box">
-                        <a className="user-sel" href="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online"><a className="icon"><FaUsers /></a>Users</a>
-                    </div>
-                    <div className="product-box">
-                        <a className="product-sel" href="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online"> <a className="icon"><MdInventory2 /></a>  Products</a>
-                    </div>
-                    <div className="profile-box">
-                        <a className="profile" href="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online"><img src={process.env.PUBLIC_URL + "/megan-fox-avatar.jpg"} alt="" />Megan Fox</a>
+                    <ul className="menu">
+                        <li className="menu-item ">
+                            <span className="icon"><FaUsers /></span>
+                            <span className="text">Users</span>
+                        </li>
+                        <li className="menu-item active">
+                            <span className="icon"><FaBoxArchive /></span>
+                            <span className="text">Products</span>
+                        </li>
+                    </ul>
+                    <div className="sidebar-user">
+                        <img src="/Megan Fox-avatar.jpg" alt="User Avatar" className="avatar" />
+                        <a href="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online" className="username">Megan Fox</a>
                     </div>
                 </div>
                 <div className="main-box">
-                    <div className="title">ADD PRODUCTS</div>
+                    <div className="title">UPDATE PRODUCTS</div>
                     <div className="content-box">
-                        <div className="infor-box">
-                            <div className="name field">
-                                <input type="text" id="name" name="name" value="Cheristie" />
-                            </div>
-                            <div className="price field">
-                                <input type="text" id="price" name="price" value="23.000 VND" />
-                            </div>
+                        <form type="text" action="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online" method="POST">
+                            <div className="infor-box">
+                                <div className="name field">
+                                    <input type="text" id="name" name="name" value="Cheristie" />
+                                </div>
+                                <div className="price field">
+                                    <input type="text" id="price" name="price" value="23.000 VND" />
+                                </div>
 
-                            <div className="time field">
-                                <input type="text" id="time" name="time" value="12/12/2025" />
+                                <div className="time field">
+                                    <input type="text" id="time" name="time" value="12/12/2025" />
+                                </div>
+                                <div className="cate field">
+                                    <label for="cate">Category:</label>
+                                    <select id="cate" name="cate">
+                                        <option value="disabled selected"></option>
+                                        <option value="bag">Bag</option>
+                                        <option value="watch">Watch</option>
+                                        <option value="antique" selected>Antique</option>
+                                        <option value="sneakers">Sneakers</option>
+                                    </select>
+                                </div>
                             </div>
-                            <div className="cate field">
-                                <label for="cate">Category:</label>
-                                <select id="cate" name="cate">
-                                    <option value="disabled selected"></option>
-                                    <option value="bag">Bag</option>
-                                    <option value="watch">Watch</option>
-                                    <option value="antique" selected>Antique</option>
-                                    <option value="sneakers">Sneakers</option>
-                                </select>
-                            </div>
-                        </div>
+                        </form>
                         <div className="image-box">
                             <div className="img-area">
-                                <a className="upload-img" href="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online">+</a>
+                                <form action="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online" method="POST">
+                                    <label class="file-upload">
+                                        +
+                                        <input className="upload-img" type="file" id="fileInput"/>
+                                    </label>
+                                </form>
                             </div>
                             <a className="refre-btn" href="https://trello.com/b/e6H5IA59/d%E1%BA%A5u-gia-online">Refresh</a>
                         </div>
@@ -61,4 +75,4 @@ function UpdateProduct() {
         </>
     )
 }
-export default UpdateProduct; 
+export default AddProduct;
