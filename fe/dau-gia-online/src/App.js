d84e8f56d28ea85bf4b8d913dbc5fbfccae4e9ab
KZero27 <phamanhkhoazero27@gmail.com>
2025-02-09 21:30:12 +0700

    Anh Khoa Update UI Shopping Cart

--- fe/dau-gia-online/src/App.js
+++ fe/dau-gia-online/src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import ShoppingCart from './component/ShoppingCart';
-import Header from './default/Header';
-import Footer from './default/Footer';
+
 
 function App() {
   return (
     <>
-      <Header/>
-      <ShoppingCart/>
-      <Footer/>
     </>
   )
 }
